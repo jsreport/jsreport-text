94ccdf05563712124137418fc05aeee58f016d1c
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2016-06-13 11:10:46 +0200

    migrate to new jsreport studio

---
+++ studio/main_dev.js
@@ -0,0 +1,12 @@
+import Properties from './Properties.js'
+import Studio from 'jsreport-studio'
+
+Studio.addPropertiesComponent('text', Properties, (entity) => entity.__entitySet === 'templates' && entity.recipe === 'text')
+
+Studio.addApiSpec({
+  template: {
+    contentType: '...',
+    fileExtension: '...',
+    contentDisposition: '...'
+  }
+})
